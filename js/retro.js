70f22b094789feababf582250df93fa8cada454f
iamcharlie <riyadhosen40@gmail.com>
2024-03-03 02:02:05 +0600

    added increment of read count

--- js/retro.js
+++ js/retro.js
@@ -1,10 +1,11 @@
-const loadPosts = async() => {
+const loadPosts = async(searchValue) => {
   const res = await fetch(
-    'https://openapi.programming-hero.com/api/retro-forum/posts'
+    `https://openapi.programming-hero.com/api/retro-forum/posts?category=${searchValue}`
   );
   const data = await res.json();
 
   const allPostsContainer = document.getElementById('all-post-container');
+  allPostsContainer.innerHTML = '';
   
   data.posts.forEach(post => {
     // console.log(post);  
@@ -14,12 +15,14 @@ const loadPosts = async() => {
               <!-- post div -->
               <div class="flex pt-8 space-y-0 p-4">
                 <!-- profile pic and active status -->
-                <div class="">
+                <div class="relative">
                   <img src="${post.image}" class="rounded-full h-20 w-20 ">
+                  <div class="absolute top-5 z-10">
                   <svg xmlns="http://www.w3.org/2000/svg" width="100px" height="100px" fill="currentColor"
-                    class="bi bi-dot relative top-1 " viewBox="0 0 16 16">
+                    class="bi bi-dot relative top-0 left-4 text-red-500 " viewBox="0 0 16 16">
                     <path d="M8 9.5a1.5 1.5 0 1 0 0-3 1.5 1.5 0 0 0 0 3z" />
                   </svg>
+                </div>
             
                 </div>
                 <div class= "space-y-4">
@@ -28,7 +31,7 @@ const loadPosts = async() => {
                     <h2>Author: ${post.author.name}</h2>
                   </div>
                   <div>
-                    <h1 class="font-bold text-xl">${post.title}</h1>
+                    <h1 id="post-title" class="font-bold text-xl">${post.title}</h1>
                   </div>
                   <div>
                     <p>${post.description}
@@ -42,7 +45,7 @@ const loadPosts = async() => {
                     </div>
                     <div class="flex gap-1 items-center">
                       <img width="30" height="30" src="https://img.icons8.com/parakeet-line/48/visible.png" alt="visible" />
-                      <p>${post.view_count}</p>
+                      <p id="view-count">${post.view_count}</p>
                     </div>
                     <div class="flex gap-1 items-center">
                       <img width="30" height="30" src="https://img.icons8.com/forma-thin/24/clock.png" alt="clock" />
@@ -52,7 +55,7 @@ const loadPosts = async() => {
                 </div>
               </div>
               <div class="flex justify-end m-4">
-                <button class="p-2 bg-green-500 rounded-full">
+                <button id="read-button" onclick ="handleReadButton()" class="p-2 bg-green-500 rounded-full">
                   <img width="24" height="24" src="https://img.icons8.com/forma-thin/24/open-envelope.png" alt="open-envelope" />
                 </button>
               </div>
@@ -111,5 +114,38 @@ const loadLatestPosts = async () => {
 
 
 
-loadPosts();
+
+const handleSearchButton = () => {
+  const searchValue = document.getElementById('search-field').value;
+  console.log(searchValue);
+  loadPosts(searchValue);
+}
+
+const readContainer = document.getElementById('selection-container');
+let count = 0;
+
+
+const handleReadButton = () => {
+  const selectionDiv = document.createElement('div');
+  selectionDiv.innerHTML = `
+  <div class="flex items-center gap-2 bg-white rounded-lg p-2">
+            <h1 class="font-bold">${document.getElementById('post-title').innerText}</h1>
+            <img width="30" height="30" src="https://img.icons8.com/parakeet-line/48/visible.png" alt="visible" />
+            <p>${document.getElementById('view-count').innerText}</p>
+          </div>
+  `;
+
+  
+  readContainer.appendChild(selectionDiv);
+  count++;
+  const countContainer = document.getElementById('count-container');
+  countContainer.innerText = count;
+
+  const readButton = document.getElementById('read-button').parentElement.parentElement;
+  console.log(readButton);
+}
+
+
+
+
 loadLatestPosts();
